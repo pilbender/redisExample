38a619fa5167d2410ddb32fee509f8c2b77c4064
Richard Scott Smith <scott.smith@isostech.com>
2016-03-11 10:52:08 -0700

    Initial commit for Redis Example project skeleton.

---
+++ src/main/java/org/raescott/App.java
@@ -0,0 +1,12 @@
+package org.raescott;
+
+/**
+ * Example of using the Redis data store.
+ *
+ * @author Richard Scott Smith <pilbender@gmail.com>
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Redis" );
+    }
+}
